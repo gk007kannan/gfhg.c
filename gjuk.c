8b151638ce7601170af9eaf7c55b3dcf3f98dad2
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-03-12 03:12:06 -0700

    Create gjuk.c

---
+++ gjuk.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+ #include<conio.h>
+void main()
+{
+  int n, numbers, i=0,Sum=0;
+  float Average;
+ clrscr();
+  printf("\nPlease Enter How many Number you want?\n");
+  scanf("%d",&n);
+ 
+  printf("\nPlease Enter the elements one by one\n");
+  do
+   {
+     scanf("%d",&numbers);
+     Sum = Sum +numbers;
+     i++;
+   }while(i<n);
+ 
+  Average = Sum/n;
+ 
+  printf("\nSum of the %d Numbers = %d",n, Sum);
+  printf("\nAverage of the %d Numbers = %.2f",n, Average);
+ getch();
+}
